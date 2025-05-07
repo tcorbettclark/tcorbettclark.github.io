25116799524e7d87d1836b267f93460ddb90a8c2
Timothy Corbett-Clark <timothy.corbettclark@gmail.com>
2025-05-07 15:23:16 +0100

    Cleaned up maths loading and rendering

--- content/js/render_maths.js
+++ content/js/render_maths.js
@@ -1,5 +1,8 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-  for (var node of document.getElementsByClassName("math inline")) {
-    katex.render(node.innerHTML, node);
+  for (var node of document.getElementsByClassName("math")) {
+    katex.render(node.innerText, node, {
+      displayMode: node.classList.contains("block"), // otherwise assume it is "inline"
+      throwOnError: false,
+    });
   }
 });
