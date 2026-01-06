2cf93f0ff07ace345d525a37e326216c76dd0b7e
Timothy Corbett-Clark <timothy@corbettclark.com>
2026-01-06 15:06:17 +0000

    Trusted-types using purify

--- content/render-code.js
+++ content/render-code.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-  hljs.highlightAll();
+  // TODO: replace with the HTML Sanitizer API once more widely available
+  // https://developer.mozilla.org/en-US/docs/Web/API/HTML_Sanitizer_API
+  const policy = trustedTypes.createPolicy("highlightjs-policy", {
+    createHTML: (input) => DOMPurify.sanitize(input, {RETURN_TRUSTED_TYPE: false}),
+  });
+  document.querySelectorAll("pre code").forEach((el) => {
+    formatted_code = hljs.highlightAuto(el.textContent);
+    el.innerHTML = policy.createHTML(formatted_code.value);
+  });
 });
